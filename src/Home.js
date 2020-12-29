afa2644190f0df1c97272fc38eb9afa355b47c43
Vlad <VladDaImpaler@gmail.com>
2020-12-29 04:42:21 -0500

    Done.

---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import { Component } from "react";
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render(){
+        return <h1>Home!</h1>
+    }
+}
+
+export default Home;
